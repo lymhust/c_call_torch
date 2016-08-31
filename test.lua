c7aa5223457b0352dae9ce1983203cba4bcbec20
Yuming LI <lymhust@gmail.com>
2016-08-31 16:47:41 +0800

    Delete test.lua

--- test.lua
+++
@@ -1,9 +0,0 @@
-print("lua script func.lua have been load ")
-model = model:cuda()
-local input = torch.CudaTensor(1, 3, 480, 640)
-local output = model:forward(input)
-print(#output)
-
-function showinfo()
-    print("welcome to  lua world ")
-end
